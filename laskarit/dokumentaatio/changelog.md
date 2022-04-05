b05afb9a942ba4099eb3afdaa8d4a9c87f606eb1
Vili Vanhala <vili.vanhala@outlook.com>
2022-04-05 16:39:30 +0300

    Viikon 3 tehtävät tehty

---
+++ laskarit/dokumentaatio/changelog.md
@@ -0,0 +1,5 @@
+## Viikko 3:
+
+- Aloitettu harjoitustyötä tehtävien mukaisella järjestyksellä
+- Luotu budget.py -tiedosto kuvaamaan tämänhetkistä budgettilogiikkaa, ja tehty tähän liittyviä testejä
+
--- laskarit/dokumentaatio/src/tests/budget_test.py
+++ laskarit/dokumentaatio/src/tests/budget_test.py
@@ -6,13 +6,18 @@ class TestBudget(unittest.TestCase):
         self.budget = Budgeting("Test")
 
     def test_budget_exists(self):
-        self.assertNotEqual(self.Budgeting, None)
+        self.assertNotEqual(self.budget, None)
 
     def test_budget_creation(self):
         self.budget.create_budget("Test User",10,2000)
-        self.assertEqual(str(self.budget), "Daily sum: 200.0")
+        self.assertEqual(str(self.budget), "Daily amount: 200.0")
 
     def test_adding_income(self):
         self.budget.create_budget("Test User",20,2000)
         self.budget.add_income(2000)
-        self.assertEqual(str(self.budget), "Daily sum: 200.0")
+        self.assertEqual(str(self.budget), "Daily amount: 200.0")
+
+    def test_adding_outcome(self):
+        self.budget.create_budget("Test User", 20, 3000)
+        self.budget.add_outcome(1000)
+        self.assertEqual(str(self.budget), "Daily amount: 200.0")
