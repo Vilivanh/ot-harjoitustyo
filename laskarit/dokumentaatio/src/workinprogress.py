50ce82b370e76e642be19f18b991545e9ded5e10
Vili Vanhala <vili.vanhala@outlook.com>
2022-04-19 23:15:28 +0300

    repositorion siivousta

---
+++ laskarit/dokumentaatio/src/workinprogress.py
@@ -0,0 +1,27 @@
+from datetime import date, datetime, timedelta
+
+Syear = 2022
+Smonth = 5
+Sday = 15
+Eyear = 2022
+Emonth = 6
+Eday = 14
+
+start = date(Syear, Smonth, Sday)
+end = date(Eyear, Emonth, Eday)
+
+class Budget2:
+    def setBudget(self, income):
+        Syear = 2022
+        Smonth = 5
+        Sday = 15
+        Eyear = 2022
+        Emonth = 6
+        Eday = 14
+
+        start = date(Syear, Smonth, Sday)
+        end = date(Eyear, Emonth, Eday)
+        length = int(end - start)
+        print(length)
+        print(income)
+        
