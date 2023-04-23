a20badf983461132892d8292c682e2559db37f03
Vili Vanhala <vili.vanhala@outlook.com>
2023-04-23 21:16:59 +0300

    fix tasks

--- tasks.py
+++ tasks.py
@@ -6,11 +6,11 @@ def start(ctx):
 
 @task
 def test(ctx):
-    ctx.run("pylint.src", pty=True)
+    ctx.run("pylint src", pty=True)
 
 @task
 def coverage(ctx):
-    ctx.run("covegage run --branch -m pytest src", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
