8bf579f9d4057c27502cef5220e3f9f4436ca3db
Vili Vanhala <vili.vanhala@outlook.com>
2023-05-10 13:56:52 +0300

    update

--- src/tests/handlers_test.py
+++ src/tests/handlers_test.py
@@ -3,20 +3,20 @@ from game.handlers import Handlers
 
 class TestHandlers(unittest.TestCase):
     def test_rank_fixer(self):
-        rank = Handlers.rank_fixer("11")
+        rank = Handlers().rank_fixer("11")
         self.assertEqual(rank, "jack")
     def test_rank_fixer_ace(self):
-        rank = Handlers.rank_fixer("14")
+        rank = Handlers().rank_fixer("14")
         self.assertEqual(rank, "ace")
     def test_rank_fixer_small(self):
-        rank = Handlers.rank_fixer("4")
+        rank = Handlers().rank_fixer("4")
         self.assertEqual(rank, "4")
     def test_rank_integer(self):
-        rank = Handlers.rank_integer("jack")
+        rank = Handlers().rank_integer("jack")
         self.assertEqual(rank, "11")
     def test_rank_integer_ace(self):
-        rank = Handlers.rank_integer("ace")
+        rank = Handlers().rank_integer("ace")
         self.assertEqual(rank, "14")
     def test_rank_integer_small(self):
-        rank = Handlers.rank_integer("4")
+        rank = Handlers().rank_integer("4")
         self.assertEqual(rank, "4")
--- src/tests/shuffle_test.py
+++ src/tests/shuffle_test.py
@@ -1,26 +1,26 @@
 import unittest
-
 from game.play import Play
+
 
 class TestShuffle(unittest.TestCase):
     def test_shuffle(self):
-        player, playing_computers = Play.deal(6)
+        player, playing_computers = Play().deal(6)
         vastaus = len(playing_computers)
         self.assertEqual(vastaus, 5)
     def test_get_right_amount_of_cards(self):
-        player, playing_computers = Play.deal(6)
+        player, playing_computers = Play().deal(6)
         number_of_cards = len(player)
         self.assertEqual(number_of_cards, 9)
     def test_get_right_amount_of_cards_with_four_players(self):
-        player, playing_computers = Play.deal(4)
+        player, playing_computers = Play().deal(4)
         number_of_cards = len(player)
         self.assertEqual(number_of_cards, 13)
     def test_get_right_amount_of_cards_for_computer(self):
-        player, playing_computers = Play.deal(6)
+        player, playing_computers = Play().deal(6)
         number_of_cards = len(playing_computers[4])
         self.assertEqual(number_of_cards, 8)
     def test_get_right_amount_of_cards_for_computer_with_four_players(self):
-        player, playing_computers = Play.deal(4)
+        player, playing_computers = Play().deal(4)
         number_of_cards = len(playing_computers[1])
         self.assertEqual(number_of_cards, 13)
 
